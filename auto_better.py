9e2efdec6b29de27a3aca11f602e2232f622133a
BenOsborn <bengrantalbertosborn@gmail.com>
2020-01-08 11:24:52 +1100

    Update

--- auto_better.py
+++ auto_better.py
@@ -3,8 +3,24 @@ from PIL import Image, ImageOps
 from time import sleep
 import numpy as np
 from pynput.mouse import Button, Controller
+from sklearn import datasets
+from sklearn import svm
+import cv2
+
+#image = cv2.imread(r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\training_data\n1.jpg")
 
 mouse = Controller()
+
+def classification(pixels):
+
+    digits = datasets.load_digits()
+
+    clf = svm.SVC(gamma=0.001, C=100)
+
+    x,y = digits.data[:-1], digits.target[:-1]
+    clf.fit(x,y)
+
+    return clf.predict(digits.data[-1])
 
 def auto_bet(x_dimension, y_dimension):
     mouse.position = (x_dimension, y_dimension)
@@ -28,33 +44,48 @@ def auto_bet(x_dimension, y_dimension):
     mouse.click(Button.left, 1)
     sleep(0.05)
 
-def main(weights1, bias1, weights2, bias2, weights3, bias3):
+def main():
 
     images = []
     for h in range(6):
-        img = gui.screenshot(region=(175, 340 + 120*h, 100, 50))
+        img = gui.screenshot(region=(175, 340 + 120*h, 60, 50))
+        img = np.array(img)
+        img = cv2.resize(img, (8, 8), interpolation=cv2.INTER_AREA)
+        img = cv2.Canny(img, 100, 200)
+        img = Image.fromarray(img)
         img = ImageOps.grayscale(img)
-        images.append(np.array(img))
+        img = np.array(img)
+        new_img = []
+        for row in img:
+            for pixel in row:
+                new_img.append(pixel / 255)
+        images.append(new_img)
 
     horse = {}
     for h in range(6):
-        horse[h + 1] = round(classification(images[h], weights1, bias1, weights2, bias2, weights3, bias3), 0)
+        horse[h + 1] = classification(images[h])
 
     horse_sorted = sorted(horse.items(), key = lambda t:t[1])
 
     value = list(horse_sorted)[0][0]
 
     if value == 1:
-        auto_bet(210, 340)
+        print("1")
+        #auto_bet(210, 340)
     elif value == 2:
-        auto_bet(210, 450)
+        print("2")
+        #auto_bet(210, 450)
     elif value == 3:
-        auto_bet(210, 570)
+        print("3")
+        #auto_bet(210, 570)
     elif value == 4:
-        auto_bet(210, 690)
+        print("4")
+        #auto_bet(210, 690)
     elif value == 5:
-        auto_bet(210, 800)
+        print("5")
+        #auto_bet(210, 800)
     elif value == 6:
-        auto_bet(210, 920)
+        print("6")
+        #auto_bet(210, 920)
 
-print(main(weight_set1, b1, weight_set2, b2, weight_set3, b3))
+main()
