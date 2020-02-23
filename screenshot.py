fa10fa640bda68feefc41b94210bf29302e0ee63
Ben Osborn <58761920+BenOsborn@users.noreply.github.com>
2020-02-23 17:06:56 +1100

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,12 +2,15 @@ import pyautogui as gui
 from pynput.keyboard import Key, Listener
 import os
 
+CWD = os.getcwd()
+TRAINDIR = os.path.join(CWD, "training_data")
+
 i = [1]
 
 try:
 
     numbers = []
-    for files in os.walk(r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\training_data"):
+    for files in os.walk(TRAINDIR):
         for file in files[2]:
             numbers.append(int(file[1:-4]))
     numbers.sort()
@@ -25,7 +28,7 @@ def screenshot(key):
         for a in range(6):
 
             img = gui.screenshot(region=(180, 340 + 121*a, 56, 48))
-            img.save(r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\training_data\n" + str(i[0]) + ".jpg")
+            img.save(os.path.join(TRAINDIR, ("n" + str(i[0]) + ".jpg"))
             i[0] += 1
 
     else:
