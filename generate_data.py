4ffccf554ccf4571fe0b3342edf48772a297eaed
Ben Osborn <58761920+BenOsborn@users.noreply.github.com>
2020-02-23 16:35:25 +1100

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import random
 
-DATADIR = r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\training_data"
+DATADIR = os.getcwd()
 CATEGORIES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"]
 
 def highlight(image_array):
@@ -13,17 +13,13 @@ def highlight(image_array):
             if image_array[y][x] < 40:
                 image_array[y][x] = 0
             else:
-                image_array[y][x] = 1 #255
+                image_array[y][x] = 1
     return image_array
 
 training_data = []
 for category in CATEGORIES:
     path = os.path.join(DATADIR, category)
-    class_num = int(category) #format(int(category), "b")
-    '''
-    while len(class_num) < 5:
-        class_num = "0" + class_num
-    '''
+    class_num = int(category)
     for img in os.listdir(path):
         try:
             img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
@@ -37,13 +33,9 @@ label = []
 for sets in training_data:
     data.append(sets[0])
     label.append(sets[1] - 1)
-    '''
-    bin_string = []
-    for char in sets[1]:
-        bin_string.append(float(char))
-    label.append(bin_string)
-    '''
+
 data = np.array(data)
 label = np.array(label)
-pickle.dump(data, open(r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\pickles\data.p", "wb"))
-pickle.dump(label, open(r"C:\Users\bengr\Documents\Programs\GTA-Auto-Better\pickles\label.p", "wb"))
+PICKLEPATH = os.path.join(DATADIR, "pickles")
+pickle.dump(data, open(os.path.join(PICKLEPATH, "data.p"), "wb"))
+pickle.dump(label, open(open(os.path.join(PICKLEPATH, "label.p"), "wb"))
